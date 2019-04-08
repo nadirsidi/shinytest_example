0ace21894a1a14455d6b53798e808f2df1adb593
Nadir Sidi <nadir.sidi@enel.com>
2019-04-08 16:35:03 -0400

    Correcting recordedDTTest expected json back to original before adding export value

--- app.R
+++ app.R
@@ -4,11 +4,11 @@ library(dplyr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
+
    # Application title
    titlePanel("shinytest Example App"),
-   
-   # Sidebar with a slider input for number of bins 
+
+   # Sidebar with a slider input for number of bins
    sidebarLayout(
       sidebarPanel(
          sliderInput("scale",
@@ -17,7 +17,7 @@ ui <- fluidPage(
                      max = 10,
                      value = 5)
       ),
-      
+
       # Show a plot of the generated distribution
       mainPanel(
          DT::dataTableOutput("dtScaled")
@@ -27,21 +27,21 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
+
   set.seed(42)
   dummyData <- data.frame(x = 1:10, y = rnorm(10, mean = 10, sd = 2))
-  
+
   scaledData <- reactive({
     dummyData[, "y"] <- dummyData[, "y"] * input$scale
     return(dummyData)
   })
-  
+
   # To actually test the DT values, we need to export them
   exportTestValues(scaledData = scaledData())
-  
+
   output$dtScaled <- DT::renderDataTable(scaledData(), rownames=F)
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 
